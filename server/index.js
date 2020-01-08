d0d9c202bdd54d300efe27bfbe9bb7c3d101625d
lulu0913 <1029390056@qq.com>
2020-01-08 11:43:34 +0800

    跨域成功

--- server/index.js
+++ server/index.js
@@ -4,9 +4,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+// 以下 配置允许跨域请求； **********一定要放在上面**********
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('X-Powered-By', ' 3.2.1')
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 
 // 后端api路由
 app.use('/api/user', userApi);
